86a32d7817bc3349a96a21aa7bdc33e2842258dd
Le-Postier <yann.richard.ry2@gmail.com>
2025-12-08 21:58:36 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -5,21 +5,21 @@ menuContainer.innerHTML = `
   <ul>
     <li><a href="index.html">Accueil</a></li>
     <li class="menu-item" data-menu="portraits">
-      <a href="#">Portraits</a>
+      <a href="portraits.html">Portraits</a>
       <ul class="submenu">
         <li><a href="portraits-ethan.html">Ethan</a></li>
         <li><a href="portraits-enzo.html">Enzo</a></li>
       </ul>
     </li>
     <li class="menu-item" data-menu="automobiles">
-      <a href="#">Automobiles</a>
+      <a href="automobiles.html">Automobiles</a>
       <ul class="submenu">
         <li><a href="japancar.html">Japancar</a></li>
         <li><a href="divers-auto.html">Divers</a></li>
       </ul>
     </li>
     <li class="menu-item" data-menu="concerts">
-      <a href="#">Concerts</a>
+      <a href="concerts.html">Concerts</a>
       <ul class="submenu">
         <li><a href="lajava.html">La Java</a></li>
       </ul>
@@ -37,26 +37,21 @@ menuItems.forEach(item => {
   let opened = false;
 
   link.addEventListener('click', e => {
-    e.preventDefault();
+    // Si le sous-menu existe et que c'est le premier clic, on ouvre juste le sous-menu
+    if (submenu && !opened) {
+      e.preventDefault();
 
-    // Si le sous-menu est déjà ouvert, naviguer vers la page principale
-    if (opened) {
-      window.location.href = link.href;
-      return;
-    }
+      // Fermer tous les autres sous-menus
+      menuItems.forEach(i => {
+        const sm = i.querySelector('.submenu');
+        if (sm && sm !== submenu) sm.style.display = 'none';
+        i.dataset.opened = false;
+      });
 
-    // Fermer tous les sous-menus ouverts
-    menuItems.forEach(i => {
-      const sm = i.querySelector('.submenu');
-      if (sm && sm !== submenu) sm.style.display = 'none';
-      i.dataset.opened = false;
-    });
-
-    // Ouvrir le sous-menu actuel
-    if (submenu) {
       submenu.style.display = 'block';
       opened = true;
       item.dataset.opened = true;
-    }
+    } 
+    // Sinon, naviguer normalement vers la page
   });
 });
